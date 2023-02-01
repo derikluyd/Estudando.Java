30154151d886a4fa698ff3d7192fa5a562355cd0
Derik Luyd <97607838+derikluyd@users.noreply.github.com>
2023-02-01 14:03:59 -0300

    Add files via upload

---
+++ public class Main {.java
@@ -0,0 +1,93 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        public class Student {
+            private String name;
+            private int age;
+            private double grade;
+            public class Student {
+                private String name;
+                private int age;
+                private double grade;
+
+                public Student(String name, int age, double grade) {
+                    this.name = name;
+                    this.age = age;
+                    this.grade = grade;
+                }
+
+                public String getName() {
+                    return name;
+                }
+
+                public void setName(String name) {
+                    this.name = name;
+                }
+
+                public int getAge() {
+                    return age;
+                }
+
+                public void setAge(int age) {
+                    this.age = age;
+                }
+
+                public double getGrade() {
+                    return grade;
+                }
+
+                public void setGrade(double grade) {
+                    this.grade = grade;
+                }
+
+                @Override
+                public String toString() {
+                    return "Student{" +
+                            "name='" + name + '\'' +
+                            ", age=" + age +
+                            ", grade=" + grade +
+                            '}';
+                }
+            }
+
+            public Student(String name, int age, double grade) {
+                this.name = name;
+                this.age = age;
+                this.grade = grade;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public int getAge() {
+                return age;
+            }
+
+            public void setAge(int age) {
+                this.age = age;
+            }
+
+            public double getGrade() {
+                return grade;
+            }
+
+            public void setGrade(double grade) {
+                this.grade = grade;
+            }
+
+            @Override
+            public String toString() {
+                return "Student{" +
+                        "name='" + name + '\'' +
+                        ", age=" + age +
+                        ", grade=" + grade +
+                        '}';
+            }
+            {
+            	
+            	
